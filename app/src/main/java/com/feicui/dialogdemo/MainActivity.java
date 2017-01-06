e937ceb1a4a8fe6dca9b688fdefb1a78c7e92562
xiaohao <xiaohao@163.com>
2017-01-06 19:12:57 +0800

    所有Dialog类型

--- app/src/main/java/com/feicui/dialogdemo/MainActivity.java
+++ app/src/main/java/com/feicui/dialogdemo/MainActivity.java
@@ -1,19 +1,25 @@
 package com.feicui.dialogdemo;
 
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -39,7 +45,10 @@ public class MainActivity extends AppCompatActivity {
                 "创建AlertDialog 普通列表对话框",
                 "创建AlertDialog 单选列表对话框",
                 "创建AlertDialog 多选列表对话框",
-                "创建AlertDialog 带图标的列表对话框"
+                "创建AlertDialog 带图标的列表对话框",
+                "创建AlertDialog 自定义对话框",
+                "创建ProgressDialog 对话框",
+                "DatePickerDialog 日期选择对话框"
         };
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, string);
@@ -66,10 +75,20 @@ public class MainActivity extends AppCompatActivity {
                 case 4:
                     simpleIconListDialog();
                     break;
+                case 5:
+                    CustomDialog();
+                    break;
+                case 6:
+                    ProgressDialogDemo();
+                    break;
+                case 7:
+                    DatePickerDialogDemo();
+                    break;
 
             }
         }
     };
+
 
     /*
     * AlertDialog的简单使用
@@ -249,4 +268,96 @@ public class MainActivity extends AppCompatActivity {
         builder.show();
 
     }
+
+
+
+    /*自定义布局文件；
+     使用LayoutInflater 的 inflater()方法填充自定义的布局文件，返回view对象。
+     用该对象的findViewById()方法加载自定义布局上所有控件；
+     调用Builder对象的setView()方法加载view对象；
+     调用Builder对象的create()方法创建AlertDialog对话框；
+     调用AlertDialog的show()方法来显示对话框。*/
+
+    private void CustomDialog() {
+        View view = null;
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setIcon(R.mipmap.ic_launcher);
+        builder.setTitle("管理员登录：");
+        LayoutInflater mInflater = LayoutInflater.from(MainActivity.this);
+        view = mInflater.inflate(R.layout.loginform_main, null);
+        final EditText edit_loginform_username = (EditText) view
+                .findViewById(R.id.edit_loginform_username);
+        final EditText edit_loginform_password = (EditText) view
+                .findViewById(R.id.edit_loginform_password);
+        builder.setView(view);
+        builder.setPositiveButton("登录", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String username = edit_loginform_username.getText()
+                        .toString().trim();
+                String password = edit_loginform_password.getText()
+                        .toString().trim();
+                if (username.length() < 1 || password.length() < 1) {
+                    textView.setText("用户名或密码不可以为空！");
+                } else {
+                    StringBuilder sb = new StringBuilder();
+                    sb.append("您的登录信息：");
+                    sb.append("用户名：");
+                    sb.append(username);
+                    sb.append("密码：");
+                    sb.append(password);
+                    textView.setText(sb.toString());
+                }
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //此处进行异步任务访问网络等处理。
+            }
+        });
+        builder.create().show();
+
+    }
+
+
+
+    /*实例化ProgressDialog，创建出ProgressDialog对象；
+    调用该对象的方法设置图标、标题、内容、按钮等；
+    setTitle()：为对话框设置标题setIcon ()：设置图标setMessage ()：设置要显示的信息
+    调用 ProgressDialog 对象的show()方法显示出 ProgressDialog 对话框。*/
+    private void ProgressDialogDemo() {
+
+        ProgressDialog pDialog = new ProgressDialog(MainActivity.this);
+        pDialog.setTitle("提示：");
+        pDialog.setMessage("数据加载中。。。");
+        pDialog.show();
+    }
+
+
+    /*创建 DatePickerDialog 对话框的步骤：
+    实例化 DatePickerDialog  ，创建出 DatePickerDialog  对象；
+    调用  DatePickerDialog  对象的show()方法显示出  DatePickerDialog  对话框；
+    绑定监听器：OnDateSetListener().*/
+    private void DatePickerDialogDemo() {
+
+        // 获取系统当前日期
+        Calendar calendar = Calendar.getInstance();
+        DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+
+                textView.setText("您选择了：" + i
+                        + ":" + (i1 + 1) + ":"
+                        + i2);
+            }
+        },calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH));
+        datePickerDialog.setCancelable(true);
+        datePickerDialog.show();
+
+    }
+
+
 }
